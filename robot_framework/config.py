d0626f5534688889aa3c0277a2339e2ff1936a9e
Laura-Salmonsen <balas@aarhus.dk>
2026-09-18 08:12:14 +0200

    Update config.py

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -19,7 +19,7 @@ ERROR_EMAIL = "Error Email"
 # ----------------------
 
 # The name of the job queue (if any)
-QUEUE_NAME = "GoSagsOpretter"
+QUEUE_NAME = "GoOpretSag"
 
 # The limit on how many queue elements to process
 MAX_TASK_COUNT = 100
